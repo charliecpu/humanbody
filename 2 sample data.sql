ff2ae0de6684a6ca7080205984702e430de41ec2
charliecpu <charliecpudev@gmail.com>
2022-11-28 22:10:19 +0000

    more sample data

--- 2 sample data.sql
+++ 2 sample data.sql
@@ -5,3 +5,7 @@ union select 'nose', 1
 union select 'mouth', 2
 union select 'teeth', 32
 union select 'toungue', 1
+
+--no you can't because last time you got a ticket
+insert body(bodypart, num)
+select 'hair', 4000
